6498ec459a76ebc41df1580afa71952293710ca8
Danny Huynh <danhuynh0803@gmail.com>
2018-02-11 22:11:35 -0500

    Reorganize test cases

--- testsrc/tests-matrix.cpp
+++ testsrc/tests-matrix.cpp
@@ -7,53 +7,65 @@ std::vector<float> vA = {
     1, 2, 3, 4,
     1, 2, 3, 4,
     1, 2, 3, 4,
-    1, 2, 3, 4
-};
+    1, 2, 3, 4};
 
 std::vector<float> vB = {
     2, 4, 6, 8,
     2, 4, 6, 8,
     2, 4, 6, 8,
-    2, 4, 6, 8
-};
+    2, 4, 6, 8};
 
 mat4 I = identity();
 mat4 Z = zero();
 mat4 A, B;
 
-TEST_CASE("Matrix Add", "[mat4 Add]")
+TEST_CASE("mat4 add/sub operator", "[mat4]")
 {
-    A = vA; 
+    A = vA;
     B = vB;
-    REQUIRE((A + A) == B);
-    REQUIRE((A + Z) == A);
+
+    SECTION("adding mat4 with mat4")
+    {
+        REQUIRE((A + A) == B);
+        REQUIRE((A + Z) == A);
+        REQUIRE(((-Z) + A) == A);
+        REQUIRE(((-A) + A) == Z);
+    }
+
+    SECTION("subtracting mat4 with mat4")
+    {
+        REQUIRE((A - A) == Z);
+        REQUIRE((B - A) == A);
+        REQUIRE((A - B) == -A);
+        REQUIRE((A - (-A)) == B);
+    }
 }
 
-TEST_CASE("Matrix Subtraction", "[mat4 Sub]")
+TEST_CASE("mat4 multiplication operator", "[mat4]")
 {
-    A = vA; 
+    A = vA;
     B = vB;
-    REQUIRE((A - A) == Z);
-    REQUIRE((B - A) == A);
-    REQUIRE((A - B) == -A);
-}
 
-TEST_CASE("Matrix-Matrix Multiplication", "[mat4-mat4 Mult]")
-{
-    A = vA; 
-    B = vB;
-    REQUIRE((I * I) == I);
-    REQUIRE((A * I) == A);
-    REQUIRE((A * Z) == Z);
-    REQUIRE((A * 2) == B);
-    REQUIRE((2 * A) == B);
-}
+    SECTION("multiplying mat4 with mat4")
+    {
+        REQUIRE((I * B) == B);
+        REQUIRE((A * I) == A);
+        REQUIRE((A * Z) == Z);
+        REQUIRE((Z * B) == Z);
+    }
 
-TEST_CASE("Matrix-Vector Multiplication", "[mat4-vec4 Mult]")
-{
-    vec4 v0(0,0,0,0);
-    vec4 v1(1,2,3,4);
-    REQUIRE((I * v1) == v1);
-    REQUIRE((Z * v1) == v0);
-}
+    SECTION("multiplying mat4 with float")
+    {
+        REQUIRE((A * 2) == B);
+        REQUIRE((2 * A) == B);
+        REQUIRE((-2 * A) == -B);
+    }
 
+    SECTION("multiplying mat4 with vec4")
+    {
+        vec4 v0(0, 0, 0, 0);
+        vec4 v1(1, 2, 3, 4);
+        REQUIRE((I * v1) == v1);
+        REQUIRE((Z * v1) == v0);
+    }
+}
