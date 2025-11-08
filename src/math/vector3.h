f3db687b31b0f88bd1ea1df0fe6cf8675e079766
Danny Huynh <danhuynh0803@gmail.com>
2025-11-08 13:05:46 -0800

    separate vec3 and vec4 to separate headers but keep master header structure

---
+++ src/math/vector3.h
@@ -0,0 +1,189 @@
+#pragma once
+
+#include <math.h>
+#include <stdlib.h>
+#include <iostream>
+
+template <typename T>
+class vec3
+{
+public:
+    vec3() { e[0] = 0.0f, e[1] = 0.0f, e[2] = 0.0f; }
+    vec3(const vec3<T>& v) { e[0] = v.e[0], e[1] = v.e[1], e[2] = v.e[2]; }
+    vec3(T f) { e[0] = f, e[1] = f, e[2] = f; }
+    vec3(T e0, T e1, T e2) { e[0] = e0, e[1] = e1, e[2] = e2; }
+
+    inline void operator =(const vec3 &v2);
+    inline const vec3<T>& operator +() const { return *this; }
+    inline vec3 operator -() const { return vec3(-e[0], -e[1], -e[2]); }
+    inline T operator [](int i) const { return e[i]; }
+    inline T& operator [](int i) { return e[i]; };
+
+    inline vec3& operator +=(const vec3 &v2);
+    inline vec3& operator -=(const vec3 &v2);
+    inline vec3& operator *=(const vec3 &v2);
+    inline vec3& operator /=(const vec3 &v2);
+    inline vec3& operator *=(const T t);
+    inline vec3& operator /=(const T t);
+
+    inline T length() const {
+        return sqrt(e[0]*e[0] + e[1]*e[1] + e[2]*e[2]); }
+    inline T squared_length() const {
+        return (e[0]*e[0] + e[1]*e[1] + e[2]*e[2]);}
+    inline void make_unit_vector();
+
+    union {
+        struct {
+            T x, y, z;
+        };
+        struct {
+            T r, g, b;
+        };
+        T e[3];
+    };
+};
+
+template <typename T>
+inline std::istream& operator>>(std::istream &is, vec3<T> &t) {
+    is >> t.e[0] >> t.e[1] >> t.e[2];
+    return is;
+}
+
+template <typename T>
+inline std::ostream& operator<<(std::ostream &os, const vec3<T> &t) {
+    os << t.e[0] << " " << t.e[1] << " " << t.e[2];
+    return os;
+}
+
+template <typename T>
+inline void vec3<T>::make_unit_vector() {
+    T k = 1.0f / sqrt(e[0]*e[0] + e[1]*e[1] + e[2]*e[2]);
+    e[0] *= k; e[1] *= k; e[2] *= k;
+}
+
+template <typename T>
+inline void vec3<T>::operator=(const vec3<T> &v2) {
+    e[0] = v2.e[0];
+    e[1] = v2.e[1];
+    e[2] = v2.e[2];
+}
+
+template <typename T>
+inline bool operator ==(const vec3<T> &v1, const vec3<T> &v2) {
+    return v1.e[0] == v2.e[0] &&
+           v1.e[1] == v2.e[1] &&
+           v1.e[2] == v2.e[2];
+}
+
+template <typename T>
+inline vec3<T> operator+(const vec3<T> &v1, const vec3<T> &v2) {
+    return vec3<T>(v1.e[0] + v2.e[0], v1.e[1] + v2.e[1], v1.e[2] + v2.e[2]);
+}
+
+template <typename T>
+inline vec3<T> operator-(const vec3<T> &v1, const vec3<T> &v2) {
+    return vec3<T>(v1.e[0] - v2.e[0], v1.e[1] - v2.e[1], v1.e[2] - v2.e[2]);
+}
+
+template <typename T>
+inline vec3<T> operator*(const vec3<T> &v1, const vec3<T> &v2) {
+    return vec3<T>(v1.e[0] * v2.e[0], v1.e[1] * v2.e[1], v1.e[2] * v2.e[2]);
+}
+
+template <typename T>
+inline vec3<T> operator/(const vec3<T> &v1, const vec3<T> &v2) {
+    return vec3<T>(v1.e[0] / v2.e[0], v1.e[1] / v2.e[1], v1.e[2] / v2.e[2]);
+}
+
+template <typename T>
+inline vec3<T> operator*(const vec3<T> &v, T t) {
+    return vec3<T>(v.e[0] * t, v.e[1] * t, v.e[2] * t);
+}
+
+// TODO
+/*
+inline vec3<int> operator*(const vec3<int> &v, int t) {
+    return vec3<int>(v.e[0] * t, v.e[1] * t, v.e[2] * t);
+}
+
+inline vec3<float> operator*(const vec3<float> &v, float t) {
+    return vec3<float>(v.e[0] * t, v.e[1] * t, v.e[2] * t);
+}
+*/
+
+template <typename T>
+inline vec3<T> operator/(vec3<T> v, T t) {
+    return vec3<T>(v.e[0] / t, v.e[1] / t, v.e[2] / t);
+}
+
+template <typename T>
+inline vec3<T> operator*(T t, const vec3<T> &v) {
+    return vec3<T>(v.e[0] * t, v.e[1] * t, v.e[2] * t);
+}
+
+template <typename T>
+inline T dot(const vec3<T> &v1, const vec3<T> &v2) {
+    return v1.e[0] * v2.e[0] + v1.e[1] * v2.e[1] + v1.e[2] * v2.e[2];
+}
+
+template <typename T>
+inline vec3<T> cross(const vec3<T> &v1, const vec3<T> &v2) {
+    return vec3<T>( (v1.e[1]*v2.e[2] - v1.e[2]*v2.e[1]),
+            (-(v1.e[0]*v2.e[2] - v1.e[2]*v2.e[0])),
+            (v1.e[0]*v2.e[1] - v1.e[1]*v2.e[0]) );
+}
+
+template <typename T>
+inline vec3<T>& vec3<T>::operator+=(const vec3<T> &v) {
+    e[0] += v.e[0];
+    e[1] += v.e[1];
+    e[2] += v.e[2];
+    return *this;
+}
+
+template <typename T>
+inline vec3<T>& vec3<T>::operator-=(const vec3<T> &v) {
+    e[0] -= v.e[0];
+    e[1] -= v.e[1];
+    e[2] -= v.e[2];
+    return *this;
+}
+
+template <typename T>
+inline vec3<T>& vec3<T>::operator*=(const vec3<T> &v) {
+    e[0] *= v.e[0];
+    e[1] *= v.e[1];
+    e[2] *= v.e[2];
+    return *this;
+}
+
+template <typename T>
+inline vec3<T>& vec3<T>::operator/=(const vec3<T> &v) {
+    e[0] /= v.e[0];
+    e[1] /= v.e[1];
+    e[2] /= v.e[2];
+    return *this;
+}
+
+template <typename T>
+inline vec3<T>& vec3<T>::operator*=(T t) {
+    e[0] *= t;
+    e[1] *= t;
+    e[2] *= t;
+    return *this;
+}
+
+template <typename T>
+inline vec3<T>& vec3<T>::operator/=(T t) {
+    e[0] /= t;
+    e[1] /= t;
+    e[2] /= t;
+    return *this;
+}
+
+template <typename T>
+inline vec3<T> unit_vector(vec3<T> v) {
+    return v / v.length();
+}
+
+
---
+++ src/math/vector4.h
@@ -0,0 +1,191 @@
+#pragma once
+
+#include <math.h>
+#include <stdlib.h>
+#include <iostream>
+
+template <typename T>
+class vec4
+{
+public:
+    vec4() { e[0] = 0.0f, e[1] = 0.0f, e[2] = 0.0f, e[3] = 0.0f; }
+    vec4(const vec3<T>& v, T w) {
+       e[0] = v[0];
+       e[1] = v[1];
+       e[2] = v[2];
+       e[3] = w;
+    }
+    vec4(T e0, T e1, T e2, T e3) { e[0] = e0, e[1] = e1, e[2] = e2, e[3] = e3; }
+    inline T x() const { return e[0]; }
+    inline T y() const { return e[1]; }
+    inline T z() const { return e[2]; }
+    inline T w() const { return e[3]; }
+    inline T r() const { return e[0]; }
+    inline T g() const { return e[1]; }
+    inline T b() const { return e[2]; }
+    inline T a() const { return e[3]; }
+    inline vec3<T> xyz() const { return vec3<T>(e[0], e[1], e[2]); }
+
+    inline void operator =(const vec4 &v2);
+    inline const vec4& operator +() const { return *this; }
+    inline vec4 operator -() const { return vec4(-e[0], -e[1], -e[2], -e[3]); }
+    inline T operator [](int i) const { return e[i]; }
+    inline T& operator [](int i) { return e[i]; };
+
+    inline vec4& operator +=(const vec4 &v2);
+    inline vec4& operator -=(const vec4 &v2);
+    inline vec4& operator *=(const vec4 &v2);
+    inline vec4& operator /=(const vec4 &v2);
+    inline vec4& operator *=(const T t);
+    inline vec4& operator /=(const T t);
+
+    inline T length() const {
+        return sqrt(e[0]*e[0] + e[1]*e[1] + e[2]*e[2]); }
+    inline T squared_length() const {
+        return (e[0]*e[0] + e[1]*e[1] + e[2]*e[2]);}
+    inline void make_unit_vector();
+
+    T e[4];
+};
+
+template <typename T>
+inline std::istream& operator>>(std::istream &is, vec4<T> &t) {
+    is >> t.e[0] >> t.e[1] >> t.e[2] >> t.e[3];
+    return is;
+}
+
+template <typename T>
+inline std::ostream& operator<<(std::ostream &os, const vec4<T> &t) {
+    os << t.e[0] << " " << t.e[1] << " " << t.e[2] << " " << t.e[3];
+    return os;
+}
+
+template <typename T>
+inline void vec4<T>::make_unit_vector() {
+    T k = 1.0f / sqrt(e[0]*e[0] + e[1]*e[1] + e[2]*e[2] + e[3]*e[3]);
+    e[0] *= k; e[1] *= k; e[2] *= k; e[3] *= k;
+}
+
+template <typename T>
+inline void vec4<T>::operator=(const vec4<T> &v2) {
+    e[0] = v2.e[0];
+    e[1] = v2.e[1];
+    e[2] = v2.e[2];
+    e[3] = v2.e[3];
+}
+
+template <typename T>
+inline bool operator ==(const vec4<T> &v1, const vec4<T> &v2) {
+    return v1.e[0] == v2.e[0] &&
+           v1.e[1] == v2.e[1] &&
+           v1.e[2] == v2.e[2] &&
+           v1.e[3] == v2.e[3];
+}
+
+template <typename T>
+inline vec4<T> operator+(const vec4<T> &v1, const vec4<T> &v2) {
+    return vec4(v1.e[0] + v2.e[0], v1.e[1] + v2.e[1], v1.e[2] + v2.e[2], v1.e[3] + v2.e[3]);
+}
+
+template <typename T>
+inline vec4<T> operator-(const vec4<T> &v1, const vec4<T> &v2) {
+    return vec4(v1.e[0] - v2.e[0], v1.e[1] - v2.e[1], v1.e[2] - v2.e[2], v1.e[3] - v2.e[3]);
+}
+
+template <typename T>
+inline vec4<T> operator*(const vec4<T> &v1, const vec4<T> &v2) {
+    return vec4(v1.e[0] * v2.e[0], v1.e[1] * v2.e[1], v1.e[2] * v2.e[2], v1.e[3] * v2.e[3]);
+}
+
+template <typename T>
+inline vec4<T> operator/(const vec4<T> &v1, const vec4<T> &v2) {
+    return vec4(v1.e[0] / v2.e[0], v1.e[1] / v2.e[1], v1.e[2] / v2.e[2], v1.e[3] / v2.e[3]);
+}
+
+template <typename T>
+inline vec4<T> operator*(const vec4<T> &v, T t) {
+    return vec4(v.e[0] * t, v.e[1] * t, v.e[2] * t, v.e[3] * t);
+}
+
+template <typename T>
+inline vec4<T> operator/(vec4<T> v, T t) {
+    return vec4(v.e[0] / t, v.e[1] / t, v.e[2] / t, v.e[3] / t);
+}
+
+template <typename T>
+inline vec4<T> operator*(T t, const vec4<T> &v) {
+    return vec4(v.e[0] * t, v.e[1] * t, v.e[2] * t, v.e[3] * t);
+}
+
+template <typename T>
+inline T dot(const vec4<T> &v1, const vec4<T> &v2) {
+    return v1.e[0] * v2.e[0] + v1.e[1] * v2.e[1] + v1.e[2] * v2.e[2] + v1.e[3] * v2.e[3];
+}
+
+// TODO
+/*
+inline vec4f cross(const vec4f &v1, const vec4f &v2) {
+    return vec4f( (v1.e[1]*v2.e[2] - v1.e[2]*v2.e[1]),
+            (-(v1.e[0]*v2.e[2] - v1.e[2]*v2.e[0])),
+            (v1.e[0]*v2.e[1] - v1.e[1]*v2.e[0]) );
+}
+*/
+
+template <typename T>
+inline vec4<T>& vec4<T>::operator+=(const vec4<T> &v) {
+    e[0] += v.e[0];
+    e[1] += v.e[1];
+    e[2] += v.e[2];
+    e[3] += v.e[3];
+    return *this;
+}
+
+template <typename T>
+inline vec4<T>& vec4<T>::operator-=(const vec4<T> &v) {
+    e[0] -= v.e[0];
+    e[1] -= v.e[1];
+    e[2] -= v.e[2];
+    e[3] -= v.e[3];
+    return *this;
+}
+
+template <typename T>
+inline vec4<T>& vec4<T>::operator*=(const vec4<T> &v) {
+    e[0] *= v.e[0];
+    e[1] *= v.e[1];
+    e[2] *= v.e[2];
+    e[3] *= v.e[3];
+    return *this;
+}
+
+template <typename T>
+inline vec4<T>& vec4<T>::operator/=(const vec4<T> &v) {
+    e[0] /= v.e[0];
+    e[1] /= v.e[1];
+    e[2] /= v.e[2];
+    e[3] /= v.e[3];
+    return *this;
+}
+
+template <typename T>
+inline vec4<T>& vec4<T>::operator*=(T t) {
+    e[0] *= t;
+    e[1] *= t;
+    e[2] *= t;
+    e[3] *= t;
+    return *this;
+}
+
+template <typename T>
+inline vec4<T>& vec4<T>::operator/=(T t) {
+    e[0] /= t;
+    e[1] /= t;
+    e[2] /= t;
+    e[3] /= t;
+    return *this;
+}
+
+template <typename T>
+inline vec4<T> unit_vector(vec4<T> v) {
+    return v / v.length();
+}
