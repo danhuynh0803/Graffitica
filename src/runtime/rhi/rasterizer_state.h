b5de6abcbeaac1c3b27baec310534d3e5b51967b
Danny Huynh <danhuynh0803@gmail.com>
2025-11-23 14:16:06 -0800

    Add depth stencil desc to later pair with rhi::pipeline state

--- src/runtime/rhi/rasterizer_state.h
+++ src/runtime/rhi/rasterizer_state.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "core/types.h"
+
 enum class CULL_MODE
 {
     CULL_MODE_NONE = 0,
@@ -11,6 +13,39 @@ enum class FILL_MODE
 {
     FILL_MODE_WIREFRAME = 0,
     FILL_MODE_SOLID,
+};
+
+enum class DEPTH_COMPARISON_FUNC
+{
+    COMPARISON_NONE = 0,
+    COMPARISON_FUNC_NEVER = 1,
+    COMPARISON_FUNC_LESS = 2,
+    COMPARISON_FUNC_EQUAL = 3,
+    COMPARISON_FUNC_LESS_EQUAL = 4,
+    COMPARISON_FUNC_GREATER = 5,
+    COMPARISON_FUNC_NOT_EQUAL = 6,
+    COMPARISON_FUNC_GREATER_EQUAL = 7,
+    COMPARISON_FUNC_ALWAYS = 8
+};
+
+enum class DEPTH_WRITE_MASK
+{
+    DEPTH_WRITE_MASK_ZERO = 0,
+    DEPTH_WRITE_MASK_ALL = 1
+};
+
+struct DepthStencilDesc
+{
+    bool                    depthEnable;
+    DEPTH_WRITE_MASK        depthWriteMask;
+    DEPTH_COMPARISON_FUNC   depthFunc;
+    bool                    stencilEnable;
+    U8                      stencilReadMask;
+    U8                      stencilWriteMask;
+
+    // TODO StencilOp Desc
+    // most likely never gonna be used by CPU case,
+    // but reminder that those fields are missing
 };
 
 struct RasterizerState
