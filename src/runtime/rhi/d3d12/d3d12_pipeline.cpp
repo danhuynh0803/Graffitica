1fbaeb13516c2e12ad67ee2a1edeea9a5daa50d0
Danny Huynh <danhuynh0803@gmail.com>
2026-04-07 00:56:20 -0700

    init d3d12 pipeline rhi

---
+++ src/runtime/rhi/d3d12/d3d12_pipeline.cpp
@@ -0,0 +1,7 @@
+#include "d3d12_pipeline.h"
+
+namespace gr::rhi::d3d12
+{
+
+
+} //namespace gr::rhi::d3d12
---
+++ src/runtime/rhi/d3d12/d3d12_pipeline.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <directx/d3dx12.h>
+#include "rhi/interface/pipeline.h"
+#include "core/types.h"
+
+namespace gr::rhi::d3d12
+{
+
+struct D3D12PipelineKey
+{
+    U32 m_VertexShaderHash;
+    U32 m_PixelShaderHash;
+};
+
+struct ConvertToKey
+{
+    U64 operator()(const D3D12PipelineKey& key)
+    {
+
+    }
+};
+
+class D3D12Pipeline final : public IPipeline
+{
+public:
+    D3D12Pipeline(const D3D12PipelineKey& key);
+    ~D3D12Pipeline() = default;
+    
+    CD3DX12_ROOT_SIGNATURE_DESC m_RootSignatureDesc;
+    D3D12_INPUT_ELEMENT_DESC m_InputElementDesc;
+    D3D12_BLEND_DESC m_BlendDesc;
+
+private:
+
+};
+
+} //namespace gr::rhi::d3d12
