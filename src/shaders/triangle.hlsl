2cbc9507ed76ee66c739ebe519d810e06e6defa3
Danny Huynh <danhuynh0803@gmail.com>
2026-04-22 01:33:37 -0700

    move shaders into src/

---
+++ src/shaders/triangle.hlsl
@@ -0,0 +1,24 @@
+struct PSInput
+{
+    float4 position : SV_POSITION;
+    float4 color : COLOR;
+    float3 normal : NORMAL;
+    float2 uv : TEXCOORD;
+};
+
+PSInput VSMain(float4 position : POSITION, float4 color : COLOR) //, float3 normal : NORMAL, float2 uv : TEXCOORD)
+{
+    PSInput result;
+
+    result.position = float4(position.xyz, 1.0);
+    result.color = color; //float4(1.0, 0.0, 1.0, 1.0);
+    //result.normal = normal;
+    //result.uv = uv; 
+
+    return result;
+}
+
+float4 PSMain(PSInput input) : SV_TARGET
+{
+    return input.color; //* float4(1.0, 0.0, 1.0f, 1.0f);
+}
