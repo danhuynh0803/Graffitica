e644221a2aa16da8b54a70478108afa906ae5113
Danny Huynh <danhuynh0803@gmail.com>
2026-05-11 23:35:38 -0700

    cpu-rt base header setup

---
+++ src/runtime/rhi/cpu/cpu_raytracing.h
@@ -0,0 +1,38 @@
+#pragma once
+
+namespace gr::rhi::cpu
+{
+
+// Base payload struct for testing
+struct Payload
+{
+    vec3f color;
+};
+
+template <typename TPayload>
+class IRayIntersection
+{
+public:
+    virtual void Main(TPayload& payload) = 0;
+};
+
+template <typename TPayload>
+class TriangleIntersection final : public IRayIntersection
+{
+public:
+    virtual void Main(TPayload& payload) override {
+
+    }
+};
+
+template <typename TPayload>
+class SphereIntersection final : public IRayIntersection
+{
+public:
+    virtual void Main(TPayload& payload) override {
+
+    }
+};
+
+
+}
