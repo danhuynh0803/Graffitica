a5e1294eb2cdb8e29de025174f2f68273275bf66
Danny Huynh <danhuynh0803@gmail.com>
2026-08-25 00:07:40 -0700

    init resourcepool util

---
+++ src/runtime/util/ResourcePool.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <vector>
+#include "core/types.h"
+
+template <typename TResource>
+class ResourcePool
+{
+public:
+    U32 Allocate(const TResource& resource)
+    {
+        U32 handle;
+        if (!m_FreeList.empty())
+        {
+            handle = m_FreeList.back();
+            m_FreeList.pop_back();
+            m_Cache[handle] = resource;
+        }
+        else
+        {
+            handle = m_Cache.size();
+            m_Cache.push_back(resource);
+        }
+        return handle;
+    }
+
+private:
+    std::vector<TResource> m_Cache;
+    std::vector<U32> m_FreeList;
+};
