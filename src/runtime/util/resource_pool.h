2cf19360cd4bece7e5536b2f2c270b16d3a70d76
Danny Huynh <danhuynh0803@gmail.com>
2026-08-25 00:39:29 -0700

    rename to match case

--- src/runtime/util/resource_pool.h
+++ src/runtime/util/resource_pool.h
@@ -7,6 +7,18 @@ template <typename TResource>
 class ResourcePool
 {
 public:
+    TResource& Get(U32 handle)
+    {
+        assert(handle < m_Cache.size());
+        return m_Cache[handle];
+    }
+
+    void Free(U32 handle)
+    {
+        assert(handle < m_Cache.size());
+        m_FreeList.push_back(handle);
+    }
+
     U32 Allocate(const TResource& resource)
     {
         U32 handle;
