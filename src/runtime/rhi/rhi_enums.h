b58a61255226a756616a9911b9cc01a2b7f220b3
Danny Huynh <danhuynh0803@gmail.com>
2026-09-09 00:46:11 -0700

    new header for rhi enums for various state

---
+++ src/runtime/rhi/rhi_enums.h
@@ -0,0 +1,38 @@
+#pragma once
+
+namespace gr::rhi
+{
+
+// TODO need another source file for these abstractions
+enum class GrFilter : uint8_t
+{
+    NEAREST = 0,
+    LINEAR,
+    CUBIC,
+    COUNT
+};
+
+enum class GrAddressMode : uint8_t
+{
+    REPEAT = 0,
+    MIRRORED_REPEAT,
+    CLAMP_TO_EDGE,
+    CLAMP_TO_BORDER,
+    MIRROR_CLAMP_TO_EDGE,
+    COUNT
+};
+
+enum class GrComparisonFunc : uint8_t
+{
+    NEVER = 0,
+    LESS,
+    EQUAL,
+    LESS_EQUAL,
+    GREATER,
+    NOT_EQUAL,
+    GREATER_EQUAL,
+    ALWAYS,
+    COUNT
+};
+
+}
